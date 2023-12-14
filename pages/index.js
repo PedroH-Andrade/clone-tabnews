f1ee31c34e299fa460a369b9045a861b7d8b18bf
pedroh-andrade <153565022+pedroh-andrade@users.noreply.github.com>
2023-12-14 01:58:57 +0000

    Adiciona scripts `linkt:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Alterando o index para testar o deploy via Vercel.</h1>
+function Home() {
+  return <h1>Alterando o index para testar o deploy via Vercel.</h1>;
 }
 
 export default Home;
