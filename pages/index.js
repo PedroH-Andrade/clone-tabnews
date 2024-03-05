c8251ccf358a8a5714207e862c7cec31570f022d
PedroH-Andrade <161954419+PedroH-Andrade@users.noreply.github.com>
2024-03-05 02:08:07 +0000

    Personalizando pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Image from 'next/image';
+import HomepageGIF from '/assets/loading.gif'
+
 function Home(){
-    return <h1>Remember why you started!</h1>
+    return <div>
+        <h1>Remember why you started! 👨‍💻</h1>
+        <Image src={HomepageGIF} alt="Página em construção" sizes="100%" style={{width: '70%',height: 'auto',}} />
+    </div>
 }
 
 export default Home;
+
