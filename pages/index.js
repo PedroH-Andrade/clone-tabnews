6fbd0a5239e1942ddbfde822063fd40568da715c
PedroH-Andrade <161954419+PedroH-Andrade@users.noreply.github.com>
2024-03-07 03:03:49 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import Image from 'next/image';
-import HomepageGIF from '/assets/loading.gif'
+import Image from "next/image";
+import HomepageGIF from "/assets/loading.gif";
 
-function Home(){
-    return <div>
-        <h1>Remember why you started! 👨‍💻</h1>
-        <Image src={HomepageGIF} alt="Página em construção" sizes="100%" style={{width: '70%',height: 'auto',}} />
+function Home() {
+  return (
+    <div>
+      <h1>Remember why you started! 👨‍💻</h1>
+      <Image
+        src={HomepageGIF}
+        alt="Página em construção"
+        sizes="100%"
+        style={{ width: "70%", height: "auto" }}
+      />
     </div>
+  );
 }
 
 export default Home;
-
